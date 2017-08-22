ff0cae755a9616a71fdc108e182f4828de66fb5e
Leonis Lamarck <lamarck.ti@gmail.com>
2017-08-21 23:30:18 -0300

    Java Web: Servlets e JSP - MVC, RequestDispatcher - pg122

---
+++ populate.sql
@@ -0,0 +1,10 @@
+INSERT INTO contatos (nome, email, endereco, dataNascimento) VALUES ("Leonis", "lamarck.ti@gmail.com", "Av. Eraldo Barros de Souza, 1196, c", "1994/01/13");
+INSERT INTO contatos (nome, email, endereco, dataNascimento) VALUES ("Amanda", "lamarck.ti@gmail.com", "Av. Eraldo Barros de Souza, 1196, c", "1994/01/13");
+INSERT INTO contatos (nome, email, endereco, dataNascimento) VALUES ("Lamarck", "lamarck.ti@gmail.com", "Av. Eraldo Barros de Souza, 1196, c", "1994/01/13");
+INSERT INTO contatos (nome, email, endereco, dataNascimento) VALUES ("Caroline", "lamarck.ti@gmail.com", "Av. Eraldo Barros de Souza, 1196, c", "1994/01/13");
+INSERT INTO contatos (nome, email, endereco, dataNascimento) VALUES ("Tapir", "lamarck.ti@gmail.com", "Av. Eraldo Barros de Souza, 1196, c", "1994/01/13");
+INSERT INTO contatos (nome, email, endereco, dataNascimento) VALUES ("Tapireco", "lamarck.ti@gmail.com", "Av. Eraldo Barros de Souza, 1196, c", "1994/01/13");
+INSERT INTO contatos (nome, email, endereco, dataNascimento) VALUES ("Dude", "lamarck.ti@gmail.com", "Av. Eraldo Barros de Souza, 1196, c", "1984/01/25");
+INSERT INTO contatos (nome, email, endereco, dataNascimento) VALUES ("qrton", "lamarck.ti@gmail.com", "Suécia, 3524-33", "1994/01/13");
+INSERT INTO contatos (nome, email, endereco, dataNascimento) VALUES ("btrhry", "lamarck.ti@gmail.com", "Nordland, 666", "1986/01/10");
+INSERT INTO contatos (nome, email, endereco, dataNascimento) VALUES ("fnrz", "darkthonr@gmail.com", "HELL AVENUE", "1994/01/13");
