e3ef899f1cde0b6149344207dd520b4f9e667840
Leonis Lamarck <lamarck.ti@gmail.com>
2017-08-21 23:30:18 -0300

    Java Web: Servlets e JSP - MVC, RequestDispatcher - pg122

---
+++ src/br/com/base/servlets/ControllerAddC.java
@@ -0,0 +1,63 @@
+package br.com.base.servlets;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.base.dao.ContatoDao;
+import br.com.base.modelo.Contato;
+
+@WebServlet("/ControllerAddC")
+public class ControllerAddC extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ControllerAddC() {
+        super();
+        
+    }
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String endereco = request.getParameter("endereco");
+		String dataEmTexto = request.getParameter("dataNascimento");
+		Calendar dataNascimento = null;
+		try{
+			Date date = new SimpleDateFormat("DD/MM/YYYY").parse(dataEmTexto);
+			dataNascimento = Calendar.getInstance();
+			dataNascimento.setTime(date);
+		}catch(ParseException e){
+			System.out.println("Erro ao converter a data.");
+		}
+		
+		Contato contato = new Contato();
+		
+		contato.setNome(nome);
+		contato.setEmail(email);
+		contato.setEndereco(endereco);
+		contato.setDataNasc(dataNascimento);
+		ContatoDao dao = new ContatoDao();
+		dao.adiciona(contato);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/contato-adicionado.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
